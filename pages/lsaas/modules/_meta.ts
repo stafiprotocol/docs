705f92577f2aee806a2b5d7392e5a5c4bddec62d
Merlin <merlin@wetez.io>
2024-11-26 17:48:15 +0800

    fix: modules typo

--- pages/lsaas/modules/_meta.ts
+++ pages/lsaas/modules/_meta.ts
@@ -3,7 +3,7 @@ export default{
   contribution:"Contribution",
   validator_selection_ai_agent:"Validator Selection AI Agent",
   point_system:"Point System",
-  ccip_modules:"CCIP Modeules",
+  ccip_modules:"CCIP Modules",
   l2_restaking_module:"L2 Restaking Module",
   blinks_module:"Blinks Module",
   frontend:"Frontend"
