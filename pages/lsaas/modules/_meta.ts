7f83fcdb6709d5c6d3895485bef057ca955d26e6
Merlin <merlin@wetez.io>
2024-11-27 14:00:58 +0800

    feat: add neutron token ics20 module

--- pages/lsaas/modules/_meta.ts
+++ pages/lsaas/modules/_meta.ts
@@ -3,7 +3,8 @@ export default{
   contribution:"Contribution",
   validator_selection_ai_agent:"Validator Selection AI Agent",
   point_system:"Point System",
-  ccip_modules:"CCIP Modules",
+  ccip_modules: "CCIP Modules",
+  neutron_token_ics20:"Neutron Token ICS20 Module",
   l2_restaking_module:"L2 Restaking Module",
   blinks_module:"Blinks Module",
   frontend:"Frontend"
